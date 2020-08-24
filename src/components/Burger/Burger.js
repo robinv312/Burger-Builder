37e53124764cfd55454b990dbe61ff27be01786b
Robin Varghese <robinv312@gmail.com>
2020-08-24 21:16:49 +0530

    added buttons to add and remove the items

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+const burger = (props) => {
+    let transformedingredients = Object.keys(props.ingredients)
+        .map(igkey => {
+            return [...Array(props.ingredients[igkey])].map((_, i) => {
+                return <BurgerIngredient key={igkey + i} type={igkey} />;
+            });
+        })
+        .reduce((arr,el)=>{
+            return arr.concat(el)
+        },[]);
+        if(transformedingredients.length===0){
+            transformedingredients=<p>Please start adding ingredients</p>
+        }
+    return (
+
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedingredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+export default burger;
