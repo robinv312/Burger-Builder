700b5fc4de5e6e52578bb7a8dbec025438357b27
Robin Varghese <robinv312@gmail.com>
2020-08-25 15:04:29 +0530

    added the backdraw

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+class Layout extends Component {
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        });
+    }
+
+    render() {
+
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer 
+                open ={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>)
+    }
+
+};
+export default Layout;
